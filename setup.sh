bf2591ed96c2cee147e6e6bb6415b3b4afe03645
Scott Cressi <scottcressi@gmail.com>
2021-08-10 07:29:16 -0400

    unneeded

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ if [ $# -eq 0 ] ; then
 
     setup_charts
     setup_statebucket
-    execute_terraform   ex. bash setup.sh execute_terraform dev somehostedzone
     """
     exit 0
 fi
@@ -33,44 +32,6 @@ setup_charts(){
     cd /var/tmp/docker-registry-ui || exit
     git pull
     git checkout 1.5.4
-}
-
-execute_terraform(){
-    if [ -z "$1" ] ; then echo environment required ; exit 1 ; fi
-    if [ -z "$2" ] ; then echo zone required ; exit 1 ; fi
-    ENV="$1"
-    ZONE="$2"
-    echo env: "$ENV"
-    echo zone: "$ZONE"
-    echo
-
-    loop(){
-    for i in "${array[@]}" ; do
-        cd "$i" || exit
-        terraform fmt
-        cd "$DIR" || exit
-    done
-    for i in "${array[@]}" ; do
-        cd "$i" || exit
-        terraform init -upgrade
-        cd "$DIR" || exit
-    done
-    for i in "${array[@]}" ; do
-        cd "$i" || exit
-        terraform validate
-        cd "$DIR" || exit
-    done
-    for i in "${array[@]}" ; do
-        cd "$i" || exit
-        terraform plan
-        cd "$DIR" || exit
-    done
-    }
-
-    array=( $(find aws/environments/"$ENV" -maxdepth 2 -mindepth 2 -type d | grep global) )
-    loop
-    array=( $(find aws/environments/"$ENV" -maxdepth 2 -mindepth 2 -type d | grep -v global) )
-    loop
 }
 
 "$@"
